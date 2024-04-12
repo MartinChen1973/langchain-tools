8414845d4c98d6bd9314b33d44fa31745dfead31
MartinChen1973 <cheny@cheny.com>
2024-04-12 20:26:28 +0800

    Bug fixed: Removed faiss dependency.
    faiss indeed is included in langchain-community.

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='langchain-tools',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     install_requires=[
         'langchain',  # If there's a central langchain package that includes most of the functionality
         'langchain-community',  # Assuming langchain_community is a separate package
         'langchain-openai',  # Assuming langchain_openai is a separate package
-        'faiss',  # Including FAISS if it's a direct dependency
         # other dependencies as necessary
     ],
     # other metadata
